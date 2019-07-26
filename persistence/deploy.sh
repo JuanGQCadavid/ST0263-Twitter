27b9a600ffb816aeace629ad9316438359fab1d3
JuanGQCadavid <jquiro12@eafit.edu.co>
2019-07-25 19:48:09 -0500

    Projecct hierachy

---
+++ persistence/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Checking if docker is installed"
+if ! [ -x "$(command -v docker)" ]; then
+    echo 'Docker is no installed.. Starting installation'
+    wget -qO- https://get.docker.com/ | sh
+else
+    echo 'Docker is installed'
+fi
+
+echo "Removing old dockers .."
+docker stop mongo-db-service
+docker rm mongo-db-service
+
+echo "Done..."
+
+echo "Removing old img ..."
+docker rmi mongo-db-img
+echo "Done."
+
+echo "Building img"
+docker build -t mongo-db-img .
+echo "Done."
+
+echo "Creating volume ..."
+docker volume mongo-db-volume
+echo "Done"
+
+echo "Creating container for web app services..."
+docker run -d --name mongo-db-service -p 27017:27017 mongo-db-img
+echo "Done."
+
