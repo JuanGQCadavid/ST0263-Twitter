017415704282c99cbe557971b9a63d399fc30816
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2019-08-18 12:52:12 -0500

    Update deploy.sh

--- persistence/deploy.sh
+++ persistence/deploy.sh
@@ -4,6 +4,9 @@ echo "Checking if docker is installed"
 if ! [ -x "$(command -v docker)" ]; then
     echo 'Docker is no installed.. Starting installation'
     wget -qO- https://get.docker.com/ | sh
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    
 else
     echo 'Docker is installed'
 fi
